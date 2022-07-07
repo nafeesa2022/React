12701b01b3cde6095bb2551fbf052f06f0a00448
nafeesans <nafeesans@integramicro.com>
2022-07-07 11:47:35 +0530

    react project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+function App() {
+  return (
+    <div >
+      {/* Let's get started */}
+      <h1> Let's get started</h1>
+      <p>This is also visible</p>
+      <ExpenseItem/>
+    </div>
+   
+  );
+}
+
+export default App;
